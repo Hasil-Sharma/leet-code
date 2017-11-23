d9e990a411d3cf896e8a8cb0b5e095b17236a72b
Hasil Sharma <hasilsharma7@gmail.com>
2017-11-22 20:16:45 -0700

    26. Remove Duplicates from Sorted Array - O(n)&O(1)

---
+++ remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,75 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        vector<int> :: iterator itr = nums.begin();
+        int prev = 0;
+        prev = (itr != nums.end()) ? *itr++ : prev;
+        while(itr != nums.end()) {
+            while( itr != nums.end() && prev == (*itr)) itr = nums.erase(itr);
+            prev = (itr != nums.end()) ? *itr++ : prev;
+        }
+        return nums.size();
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for(int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        
+        int ret = Solution().removeDuplicates(nums);
+
+        string out = integerVectorToString(nums, ret);
+        cout << out << endl;
+    }
+    return 0;
+}
