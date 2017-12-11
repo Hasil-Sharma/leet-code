e4367c5027b416011923b43e919456aec7d19896
Hasil Sharma <hasilsharma7@gmail.com>
2017-12-10 22:26:41 -0700

    387. First Unique Character in a String - O(n) & O(1)

---
+++ first-unique-character-in-a-string-2.cpp
@@ -0,0 +1,60 @@
+// https://leetcode.com/problems/first-unique-character-in-a-string/description/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, pair<int, int>> char_map;
+        int idx = s.size();
+        
+        for(int i = 0; i < s.size(); i++) {
+            char_map[s[i]].first = i;
+            char_map[s[i]].second++;
+        }
+        
+        for(auto& itr: char_map)
+            idx = (itr.second.second == 1) ? min(idx, itr.second.first) : idx;
+        
+        return (idx == s.size()) ? -1 : idx;
+    }
+};
+
+string stringToString(string input) {
+    assert(input.length() >= 2);
+    string result;
+    for (int i = 1; i < input.length() -1; i++) {
+        char currentChar = input[i];
+        if (input[i] == '\\') {
+            char nextChar = input[i+1];
+            switch (nextChar) {
+                case '\"': result.push_back('\"'); break;
+                case '/' : result.push_back('/'); break;
+                case '\\': result.push_back('\\'); break;
+                case 'b' : result.push_back('\b'); break;
+                case 'f' : result.push_back('\f'); break;
+                case 'r' : result.push_back('\r'); break;
+                case 'n' : result.push_back('\n'); break;
+                case 't' : result.push_back('\t'); break;
+                default: break;
+            }
+            i++;
+        } else {
+          result.push_back(currentChar);
+        }
+    }
+    return result;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        string s = stringToString(line);
+        
+        int ret = Solution().firstUniqChar(s);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
