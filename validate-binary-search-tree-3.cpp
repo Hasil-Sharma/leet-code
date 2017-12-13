4489fcc817324eda6f4159013f94ba569298b502
Hasil Sharma <hasilsharma7@gmail.com>
2017-12-13 00:27:05 -0700

    98. Validate Binary Search Tree - O(n) & O(n)
    Iterative and Space Optimized

---
+++ validate-binary-search-tree-3.cpp
@@ -0,0 +1,117 @@
+// https://leetcode.com/problems/validate-binary-search-tree/description/
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        stack<TreeNode*> stk;
+        bool flag = true;
+        int prev_val, curr_val;
+        
+        while(root != NULL || !stk.empty()){
+            while(root){
+               stk.push(root);
+               root = root->left;
+            }
+
+            root = stk.top(); stk.pop();
+            curr_val = root->val;
+            if (flag) {
+                prev_val = curr_val;
+                flag = false;
+            } else if(curr_val <= prev_val) 
+                return false;
+            
+            prev_val = curr_val;
+            root = root->right;
+            
+        }
+    
+        return true;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+string boolToString(bool input) {
+    return input ? "True" : "False";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root = stringToTreeNode(line);
+        
+        bool ret = Solution().isValidBST(root);
+
+        string out = boolToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
