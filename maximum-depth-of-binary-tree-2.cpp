90e843ad0adbecd923be55dfec068d59eaf4cb01
Hasil Sharma <hasilsharma7@gmail.com>
2017-12-14 17:54:27 -0700

    104. Maximum Depth of Binary Tree - O(n) & O(n)
    Iterative

---
+++ maximum-depth-of-binary-tree-2.cpp
@@ -0,0 +1,103 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        int ans = 0;
+        stack<pair<TreeNode*, int>> stk;
+        pair<TreeNode*, int> elem;
+        stk.push({root, 0});
+        
+        while(!stk.empty()){
+            elem = stk.top(); stk.pop();
+            ans = max(ans, elem.second);
+            if (!elem.first) continue;
+            stk.push({elem.first->left, elem.second + 1});
+            stk.push({elem.first->right, elem.second + 1});
+        }
+        
+        return ans;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root = stringToTreeNode(line);
+        
+        int ret = Solution().maxDepth(root);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
