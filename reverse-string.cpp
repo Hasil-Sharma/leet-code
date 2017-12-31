40ab6b4adcd41bccb9e525e608054db742c2b7e1
Hasil Sharma <hasilsharma7@gmail.com>
2017-12-31 14:05:16 -0700

    344. Reverse String - O(n) & O(n)

---
+++ reverse-string.cpp
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/reverse-string/description/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reverseString(string s) {
+        stringstream ss;
+        for(int i = s.length() -1; i >= 0; i--){
+            ss << s[i];
+        }
+        return ss.str();
+    }
+};
+
+string stringToString(string input) {
+    assert(input.length() >= 2);
+    string result;
+    for (int i = 1; i < input.length() -1; i++) {
+        char currentChar = input[i];
+        if (input[i] == '\\') {
+            char nextChar = input[i+1];
+            switch (nextChar) {
+                case '\"': result.push_back('\"'); break;
+                case '/' : result.push_back('/'); break;
+                case '\\': result.push_back('\\'); break;
+                case 'b' : result.push_back('\b'); break;
+                case 'f' : result.push_back('\f'); break;
+                case 'r' : result.push_back('\r'); break;
+                case 'n' : result.push_back('\n'); break;
+                case 't' : result.push_back('\t'); break;
+                default: break;
+            }
+            i++;
+        } else {
+          result.push_back(currentChar);
+        }
+    }
+    return result;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        string s = stringToString(line);
+        
+        string ret = Solution().reverseString(s);
+
+        string out = (ret);
+        cout << out << endl;
+    }
+    return 0;
+}
