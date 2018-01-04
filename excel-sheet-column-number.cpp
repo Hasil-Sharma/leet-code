d656039e1e492d300949c2d7667b27b464780a3b
Hasil Sharma <hasilsharma7@gmail.com>
2018-01-04 16:50:24 -0700

    171. Excel Sheet Column Number: O(n) & O(1)

---
+++ excel-sheet-column-number.cpp
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/excel-sheet-column-number/description/
+
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int result = 0;
+        for(int i = 0; i < s.length(); result = result * 26 + s[i++] - 'A' + 1);
+        return result;
+    }
+};
+
+string stringToString(string input) {
+    assert(input.length() >= 2);
+    string result;
+    for (int i = 1; i < input.length() -1; i++) {
+        char currentChar = input[i];
+        if (input[i] == '\\') {
+            char nextChar = input[i+1];
+            switch (nextChar) {
+                case '\"': result.push_back('\"'); break;
+                case '/' : result.push_back('/'); break;
+                case '\\': result.push_back('\\'); break;
+                case 'b' : result.push_back('\b'); break;
+                case 'f' : result.push_back('\f'); break;
+                case 'r' : result.push_back('\r'); break;
+                case 'n' : result.push_back('\n'); break;
+                case 't' : result.push_back('\t'); break;
+                default: break;
+            }
+            i++;
+        } else {
+          result.push_back(currentChar);
+        }
+    }
+    return result;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        string s = stringToString(line);
+        
+        int ret = Solution().titleToNumber(s);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
