26beeb6a4ea2609817c12aa2cdcb777a703e8679
Hasil Sharma <hasilsharma7@gmail.com>
2018-01-04 22:47:32 -0700

    191. Number of 1 Bits - O(logn) & O(1)

---
+++ number-of-1-bits.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/number-of-1-bits/description/
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        while(n){
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+    }
+};
