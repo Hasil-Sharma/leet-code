c6d5379c81f19ac3b6c8f18c0b090e0d9f8fc77d
Hasil Sharma <hasilsharma7@gmail.com>
2018-02-07 21:55:29 -0700

    72. Edit Distance - O(mn) & O(mn)

---
+++ edit-distance.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/edit-distance/description/
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.length(), n = word2.length();
+        int dp[m+1][n+1];
+        
+        for(int i = 0; i <= m; i++){
+            for(int j = 0; j <= n; j++){
+                if (i == 0){
+                    // string1 is empty
+                    dp[i][j] = j;
+                } else if (j == 0){
+                    dp[i][j] = i;
+                } else if (word1[i - 1] == word2[j - 1]){
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j]));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
