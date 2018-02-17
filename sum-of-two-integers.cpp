6581b068bdb2de473e2641ef26e43c861ae00cad
Hasil Sharma <hasilsharma7@gmail.com>
2018-02-16 22:47:31 -0700

    371. Sum of Two Integers - O(sizeof(int)) & O(sizeof(int))

---
+++ sum-of-two-integers.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/sum-of-two-integers/description/
+class Solution {
+public:
+    int getSum(int a, int b) {
+        
+        return b == 0 ? a : getSum(a ^ b, (a & b) << 1);
+    }
+};
