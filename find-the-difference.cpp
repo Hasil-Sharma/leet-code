bbf1bde39ad89f138082b48e0012a1f52a45ec49
Hasil Sharma <hasilsharma7@gmail.com>
2018-03-23 23:52:42 -0600

    389. Find the Difference - O(|S| + |T|) & O(1)

---
+++ find-the-difference.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/find-the-difference/description/
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char r = 0;
+        for(char c : s) r ^= c;
+        for(char c : t) r ^= c;
+        return r;
+    }
+};
