df0cd8ed8b14361eb77253bafd0b4260501fab1d
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-07-06 19:40:00 -0700

    513. Find Bottom Left Tree Value - O(n) & O(L)
    L: width of the tree

---
+++ find-bottom-left-tree-value-3.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/find-bottom-left-tree-value/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            root = q.front();
+            q.pop();
+            if (root->right)
+                q.push(root->right);
+            if (root->left)
+                q.push(root->left);
+        }
+        return root->val;
+    }
+};
