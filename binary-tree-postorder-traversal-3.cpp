e883c4f88fffbf81e10dac402734198dcba44a6b
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-07-06 18:29:05 -0700

    145. Binary Tree Postorder Traversal - O(n) & O(h)

---
+++ binary-tree-postorder-traversal-3.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> answer;
+        stack<TreeNode*> stk;
+        
+        while(!stk.empty() || root){
+            
+            while(root){
+                if(root->right)
+                    stk.push(root->right);
+                stk.push(root);
+                root = root->left;
+            }
+            
+            root = stk.top();
+            stk.pop();
+            
+            if (root->right && !stk.empty() && root->right == stk.top()){
+                stk.pop();
+                stk.push(root);
+                root = root->right;
+            } else {
+                answer.push_back(root->val);
+                root = NULL;
+            }
+        }
+        
+        return answer;
+    }
+    
+};
