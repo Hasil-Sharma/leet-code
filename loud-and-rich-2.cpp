f72f3e845a409431639d387a6b7a390af3cb054a
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-07-08 15:36:08 -0700

    851. Loud and Rich - O(n^2) & O(n^2)

---
+++ loud-and-rich-2.cpp
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/loud-and-rich/description/
+
+class Solution {
+public:
+    typedef vector<vector<int>> vvi;
+    typedef vector<int> vi;
+    vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
+        int N = quiet.size();
+        vi answer(N, -1);
+        vvi adjList(N, vector<int>());
+        
+        for(vi edge : richer)
+            adjList[edge[1]].push_back(edge[0]);
+            
+        
+        stack<int> stk;
+        
+        for(int p = 0; p < N; p++){
+            if (answer[p] == -1){
+                stk.push(p);
+                
+                while(!stk.empty()){
+                    int n = stk.top();
+                    if (answer[n] == -1)
+                        answer[n] = n;
+                    
+                    for(int child : adjList[n]){
+                        if (answer[child] == -1)
+                            stk.push(child);
+                        else answer[n] = quiet[answer[n]] > quiet[answer[child]] ? answer[child] : answer[n];
+                    }
+                    
+                    if (n == stk.top())
+                        stk.pop();
+                }
+            }
+        }
+        
+        return answer;
+    }
+};
