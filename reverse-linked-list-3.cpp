633139c5159eddbfebbaba4a666c59791e08b054
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-07-31 17:05:48 -0700

    206. Reverse Linked List - O(n) & O(1)

---
+++ reverse-linked-list-3.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/reverse-linked-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev = NULL, *next = NULL;
+        
+        while(head){
+            next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        
+        return prev;
+    }
+};
