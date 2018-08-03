c7b84ebfe4ac776fb48a561cb6bb89b64b6bc6cc
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-08-02 20:31:34 -0700

    525. Contiguous Array - O(n) & O(n)

---
+++ contiguous-array-2.cpp
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/contiguous-array/description/
+
+#define mp(A, B) make_pair(A, B)
+#define ff(i, m, n) for(i = m; i < n; i++)
+#define ffr(i, m, n) for(i = m; i >= n; i--)
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<char>> vvc;
+typedef vector<pair<int, int>> vpii;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<string>> vvs;
+typedef stack<int> stki;
+typedef queue<int> qi;
+typedef deque<int> dqi;
+typedef queue<pair<int, int>> qpii;
+typedef unordered_map<int, int> umpii;
+typedef set<int> si;
+typedef unordered_set<int> usi;
+vpii steps = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+template <typename T>
+void printVec(T const& t){
+    for(auto e :  t)
+        cout << e << ",";
+    cout << endl;
+}
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        umpii um;
+        um[0] = -1;
+        int p = 0, ans = 0;
+        int i;
+        ff(i, 0, nums.size()){
+            p += nums[i] ? 1 : -1;
+            if (um.count(p)) ans = max(ans, i - um[p]);
+            else um[p] = i;
+        }
+        return ans;
+    }
+};
