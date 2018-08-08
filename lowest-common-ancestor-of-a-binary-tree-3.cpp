afb906b106c7bc49aab386af38bcd758484b4640
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-08-07 18:43:17 -0700

    236. Lowest Common Ancestor of a Binary Tree - O(n) & O(h)

---
+++ lowest-common-ancestor-of-a-binary-tree-3.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || root == p || root == q) return root;
+        auto left = lowestCommonAncestor(root->left, p, q);
+        auto right = lowestCommonAncestor(root->right, p, q);
+        if (left && right) return root;
+        else if (left) return left;
+        else return right;
+        
+    }
+};
