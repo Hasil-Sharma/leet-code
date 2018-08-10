43c8ec0635546ab4b05f35af26a60ac4e05f65e0
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-08-09 19:55:34 -0700

    611. Valid Triangle Number - O(n^2) & O(1)

---
+++ valid-triangle-number-2.cpp
@@ -0,0 +1,51 @@
+// https://leetcode.com/problems/valid-triangle-number/description/
+
+#define mp(A, B) make_pair(A, B)
+#define ff(i, m, n) for(i = m; i < n; i++)
+#define ffr(i, m, n) for(i = m; i >= n; i--)
+#define ss(v) sort(v.begin(), v.end())
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<char>> vvc;
+typedef vector<pair<int, int>> vpii;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<string>> vvs;
+typedef stack<int> stki;
+typedef queue<int> qi;
+typedef deque<int> dqi;
+typedef queue<pair<int, int>> qpii;
+typedef unordered_map<int, int> umpii;
+typedef unordered_map<int, vi> umpiv;
+typedef set<int> si;
+typedef unordered_set<int> usi;
+vpii steps = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+template <typename T>
+void printVec(T const& t){
+    for(auto e :  t)
+        cout << e << ",";
+    cout << endl;
+}
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        ss(nums);
+        int ans = 0;
+        int i,j,k;
+        ffr(i, nums.size() - 1, 2){
+            int l = 0, r = i - 1;
+            while (l < r){
+                if (nums[l] + nums[r] > nums[i]){
+                    ans += r - l;
+                    r--;
+                } else l++;
+            }
+        }
+        return ans;
+    }
+    
+};
