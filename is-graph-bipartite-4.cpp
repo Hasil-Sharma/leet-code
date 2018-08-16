b563ce5bafeed8fa5a501c0999853113f748f102
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-08-15 20:45:32 -0700

    785. Is Graph Bipartite? - O(V + E) & O(V)

---
+++ is-graph-bipartite-4.cpp
@@ -0,0 +1,64 @@
+// https://leetcode.com/problems/is-graph-bipartite/description/
+
+#define mp(A, B) make_pair(A, B)
+#define ff(i, m, n) for(i = m; i < n; i++)
+#define ffr(i, m, n) for(i = m; i >= n; i--)
+#define ss(v) sort(v.begin(), v.end())
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<char>> vvc;
+typedef vector<pair<int, int>> vpii;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<string>> vvs;
+typedef stack<int> stki;
+typedef queue<int> qi;
+typedef deque<int> dqi;
+typedef queue<pair<int, int>> qpii;
+typedef unordered_map<int, int> umpii;
+typedef unordered_map<int, vi> umpiv;
+typedef set<int> si;
+typedef unordered_set<int> usi;
+vpii steps = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+template <typename T>
+void printVec(T const& t){
+    for(auto e :  t)
+        cout << e << ",";
+    cout << endl;
+}
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vi color(n, -1);
+        for(int i = 0; i < n; i++){
+            if (color[i] != -1) continue;
+            color[i] = 0;
+            if (!fillColor(graph, color, i)) return false;
+        }
+        return true;
+    }
+    
+    bool fillColor(vvi& graph, vi& color, int p){
+        bool flag = true;
+        stack<int> stk;
+        stk.push(p);
+        while(!stk.empty()){
+            p = stk.top();
+            stk.pop();
+            for(int c : graph[p]){
+                if (color[c] == (!color[p])) continue;
+                else if (color[c] != -1) return false;
+                else {
+                    color[c] = (!color[p]);
+                    stk.push(c);
+                }
+            }
+        }
+        return true;
+    }
+};
