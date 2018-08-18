4cc864dd0194b70e955ff25e9bf6fb0a059f0534
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-08-18 15:36:02 -0700

    269. Alien Dictionary - O(V + E) & O(V)
    V : total unique letters in the input
    n : number of words in the input
    E : n - 1

---
+++ alien-dictionary-1.cpp
@@ -0,0 +1,99 @@
+// https://leetcode.com/problems/alien-dictionary/description/
+
+#define mp(A, B) make_pair(A, B)
+#define ff(i, m, n) for(i = m; i < n; i++)
+#define ffr(i, m, n) for(i = m; i >= n; i--)
+#define ss(v) sort(v.begin(), v.end())
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<char>> vvc;
+typedef vector<pair<int, int>> vpii;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<string>> vvs;
+typedef stack<int> stki;
+typedef queue<int> qi;
+typedef deque<int> dqi;
+typedef queue<pair<int, int>> qpii;
+typedef unordered_map<int, int> umpii;
+typedef unordered_map<int, vi> umpiv;
+typedef set<int> si;
+typedef unordered_set<int> usi;
+vpii steps = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+template <typename T>
+void printVec(T const& t){
+    for(auto e :  t)
+        cout << e << ",";
+    cout << endl;
+}
+
+class Solution {
+public:
+    string alienOrder(vector<string>& words) {
+        unordered_map<char, vc> adjList;
+        unordered_map<char, int> indegree;
+        unordered_set<char> visit;
+        stack<char> stk;
+        string ans = "";
+        
+        int n = words.size(), i;
+        ff(i, 0, n)
+            adjListGen(adjList, words, i);
+        indegreeGen(adjList, indegree);
+        
+        while(ans.length() != indegree.size()){
+            for(auto kv : indegree){
+                if (kv.second == 0 && !visit.count(kv.first)){
+                    stk.push(kv.first);
+                    ans = ans + string(1, kv.first);
+                    visit.insert(kv.first);
+                }
+            }
+            
+            if (stk.empty()) return "";
+            
+            while(!stk.empty()){
+                char c = stk.top();
+                stk.pop();
+                for (int v : adjList[c])
+                    indegree[v]--;
+            }
+        }
+        return ans;
+    }
+    
+    void adjListGen(unordered_map<char, vc>& adjList, vs& words, int i){
+        int m = words[i].length(), n = words[i+1].length(), j;
+        
+        ff(j, 0, min(m,n)){
+            if (i + 1 < words.size() && words[i][j] != words[i+1][j]){
+                adjList[words[i][j]].push_back(words[i+1][j]);
+                break;
+            }
+        }
+        
+        // adding the characters for both the words
+        for(char c : words[i]){
+            if(!adjList.count(c))
+                adjList[c];
+        }
+        
+        if (i + 1 == words.size() - 1)
+        for(char c : words[i+1]){
+            if (!adjList.count(c))
+                adjList[c];
+        }
+    }
+    
+    void indegreeGen(unordered_map<char, vc>& adjList, unordered_map<char, int>& indegree){
+        for(auto kv: adjList){
+            if (!indegree.count(kv.first)) indegree[kv.first] = 0;
+            for(auto v : kv.second)
+                indegree[v]++;
+        }
+    }
+};
