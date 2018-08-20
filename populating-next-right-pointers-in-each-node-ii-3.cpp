db96ababa5615368a8434afe2559afe031b00e56
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-08-20 13:47:14 -0700

    117. Populating Next Right Pointers in Each Node II - O(n) & O(1)

---
+++ populating-next-right-pointers-in-each-node-ii-3.cpp
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/description/
+
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        TreeLinkNode *head = NULL;
+        while(root) {
+            
+            if (root->left || root->right) {
+                if (root->left){
+                    if (!head) head = root->left;
+                    if (root->right) 
+                        root->left->next = root->right;
+                    else attach(root->left, root);
+                }
+
+                if (root->right){
+                    if (!head) head = root->right;
+                    attach(root->right, root);
+                }
+            }
+            
+            root = root->next;
+            if (root == NULL){
+                root = head;
+                head = NULL;
+            }
+        }
+    }
+    
+    void attach(TreeLinkNode* node, TreeLinkNode* root){
+        while(root->next && !node->next){
+            root = root->next;
+            if (root->left || root->right)
+                node->next = root->left ? root->left : root->right;
+        }
+    }
+};
