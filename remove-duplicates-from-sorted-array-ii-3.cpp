f8f383d8aad0a0e818818f595cf7a46ad619e35d
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-08-20 14:35:44 -0700

    80. Remove Duplicates from Sorted Array II - O(n) & O(1)

---
+++ remove-duplicates-from-sorted-array-ii-3.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for(int n : nums){
+            if (i < 2 || n > nums[i-2])
+                nums[i++] = n;
+        }
+        return i;
+    }
+};
