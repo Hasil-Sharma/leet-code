94aab2d0ab7899754f01f7848fd4a6280e797272
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-08-22 00:38:29 -0700

    560. Subarray Sum Equals K - O(n) & O(n)
    n : size of nums

---
+++ subarray-sum-equals-k-1.cpp
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/subarray-sum-equals-k/description/
+
+#define mp(A, B) make_pair(A, B)
+#define ff(i, m, n) for(i = m; i < n; i++)
+#define ffr(i, m, n) for(i = m; i >= n; i--)
+#define ss(v) sort(v.begin(), v.end())
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<char>> vvc;
+typedef vector<pair<int, int>> vpii;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<string>> vvs;
+typedef stack<int> stki;
+typedef queue<int> qi;
+typedef deque<int> dqi;
+typedef queue<pair<int, int>> qpii;
+typedef unordered_map<int, int> umpii;
+typedef unordered_map<int, vi> umpiv;
+typedef set<int> si;
+typedef unordered_set<int> usi;
+vpii steps = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+template <typename T>
+void printVec(T const& t){
+    for(auto e :  t)
+        cout << e << ",";
+    cout << endl;
+}
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        umpii preSum;
+        preSum[0] = 1;
+        int ans = 0, p = 0;
+        for(int num : nums){
+            p += num;
+            if (preSum.count(p - k))
+                ans += preSum[p - k];
+            preSum[p]++;
+        }
+        return ans;
+    }
+};
