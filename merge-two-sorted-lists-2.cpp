6bf9c9ffabcef861557e19b4adcc5a8c4331c249
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-09-08 16:24:23 -0600

    21. Merge Two Sorted Lists - O(m + n) & O(1)

---
+++ merge-two-sorted-lists-2.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* ans = new ListNode(-1), *head = ans;
+        
+        while(l1 || l2){
+            if (l1 && l2){
+                if (l1->val < l2->val) {
+                    ans->next = l1;
+                    l1 = l1->next;
+                } else {
+                    ans->next = l2;
+                    l2 = l2->next;
+                }
+            } else {
+                l1 = l1 ? l1 : l2;
+                l2 = NULL;
+                ans->next = l1;
+                l1 = l1->next;
+            }
+            ans = ans->next;
+            ans->next = NULL;
+        }
+        
+        return head->next;
+    }
+};
