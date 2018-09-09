bbc00199ec73934b7198cd6d4e49c5e3f06fbb44
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-09-08 21:45:29 -0600

    235. Lowest Common Ancestor of a Binary Search Tree - O(h) & O(1)

---
+++ lowest-common-ancestor-of-a-binary-search-tree-5.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (p->val > q->val) swap(p, q);
+        
+        while(root){
+            if (root->val > q->val)
+                root = root->left;
+            else if (root->val < p->val)
+                root = root->right;
+            else return root;
+        }
+        return root;
+    }
+};
