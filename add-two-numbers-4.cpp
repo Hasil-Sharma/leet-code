472bcc922a4512c687bfab01119c05c795074e78
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-09-10 22:53:25 -0600

    2. Add Two Numbers - O(m) & O(1)
    m : larger of the two linked list

---
+++ add-two-numbers-4.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/add-two-numbers/description/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* ans = new ListNode(-1), *head = ans;
+        int carry = 0;
+        while(l1 || l2){
+            head->next = new ListNode((l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry);
+            carry = head->next->val/10;
+            head->next->val %= 10;
+            l1 = l1 ? l1->next : NULL;
+            l2 = l2 ? l2->next : NULL;
+            head = head->next;
+        }
+        if (carry) head->next = new ListNode(carry);
+        return ans->next;
+    }
+};
