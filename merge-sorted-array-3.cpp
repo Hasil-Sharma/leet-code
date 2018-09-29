2b9b92a0f6f4aba2c1ebdfb537fa70168d51790c
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-09-28 18:21:59 -0600

    88. Merge Sorted Array - O(max(m,n)) & O(1)

---
+++ merge-sorted-array-3.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/merge-sorted-array/description/
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int k = m + n - 1, i = m - 1, j = n - 1;
+        
+        while(i >= 0 && j >= 0){
+            nums1[k--] = max(nums1[i], nums2[j]);
+            if (nums1[i] > nums2[j]) i--;
+            else j--;
+        }
+        
+        while(i >= 0)
+            nums1[k--] = nums1[i--];
+        while(j >= 0)
+            nums1[k--] = nums2[j--];
+    }
+};
