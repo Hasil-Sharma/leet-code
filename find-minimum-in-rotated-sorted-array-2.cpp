18c924604bf825c7fcf414b698b1bf86d5211ab2
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-10-05 01:13:35 -0600

    153. Find Minimum in Rotated Sorted Array - O(logn) & O(1)

---
+++ find-minimum-in-rotated-sorted-array-2.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int s = 0, e = nums.size() - 1;
+        
+        while(s < e){
+            int m = s + (e - s) / 2;
+            if (m > s && nums[m] < nums[m-1]) return nums[m];
+            else if (m < e && nums[m] > nums[m+1]) return nums[m+1];
+            else if (nums[s] < nums[m]) s = m + 1;
+            else e = m - 1;
+        }
+        
+        return nums[0];
+    }
+};
