019a77b3401ccc5dc150befebc3d0cfcf56b67ee
Hasil Sharma <hasilsharma7@gmail.com>
2018-10-20 16:03:18 -0600

    328. Odd Even Linked List - O(n) & O(1)

---
+++ odd-even-linked-list-2.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/odd-even-linked-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head) return head;
+        
+        ListNode* odd = head, *even = head->next, *evenHead = even;
+        
+        while(even && even->next){
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        
+        odd->next = evenHead;
+        return head;
+    }
+};
