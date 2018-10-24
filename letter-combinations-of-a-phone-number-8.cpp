1de826120ea85d2d26ad318746043d6ce06dd70f
Hasil Sharma <hasilsharma7@gmail.com>
2018-10-23 21:37:54 -0600

    17. Letter Combinations of a Phone Number - O(a^n) & O(n)

---
+++ letter-combinations-of-a-phone-number-8.cpp
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+
+#define mp(A, B) make_pair(A, B)
+#define ff(i, m, n) for(i = m; i < n; i++)
+#define ffr(i, m, n) for(i = m; i >= n; i--)
+#define ss(v) sort(v.begin(), v.end())
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<char>> vvc;
+typedef vector<pair<int, int>> vpii;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<string>> vvs;
+typedef stack<int> stki;
+typedef queue<int> qi;
+typedef deque<int> dqi;
+typedef queue<pair<int, int>> qpii;
+typedef unordered_map<int, int> umpii;
+typedef unordered_map<int, vi> umpiv;
+typedef set<int> si;
+typedef unordered_set<int> usi;
+vpii steps = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        unordered_map<char, vc> numToChar;
+        char d = 'a';
+        
+        for(char c = '2'; c <= '9'; c++){
+            int count = 3 + (c == '7' || c == '9');
+            while (count--)
+                numToChar[c].push_back(d++);
+        }
+        
+        vc temp;
+        vs ans;
+        if (digits.size())
+            generate(digits, temp, ans, numToChar);
+        return ans;
+    }
+    
+    void generate(string& digits, vc& temp, vs& ans, unordered_map<char, vc>& numToChar){
+        if (temp.size() == digits.length())
+            ans.push_back(string(temp.begin(), temp.end()));
+        else {
+            for (char c : numToChar[digits[temp.size()]]){
+                temp.push_back(c);
+                generate(digits, temp, ans, numToChar);
+                temp.pop_back();
+            }
+        }
+    }
+};
