02dcccb9882b647a87af107a7ca41c1226ce6fd5
Hasil Sharma <hasilsharma7@gmail.com>
2018-10-30 01:27:36 -0600

    102. Binary Tree Level Order Traversal - O(n) && O(W)
    W - width of the binary tree

---
+++ binary-tree-level-order-traversal-6.cpp
@@ -0,0 +1,65 @@
+// https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+
+#define mp(A, B) make_pair(A, B)
+#define ff(i, m, n) for(i = m; i < n; i++)
+#define ffr(i, m, n) for(i = m; i >= n; i--)
+#define ss(v) sort(v.begin(), v.end())
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<char>> vvc;
+typedef vector<pair<int, int>> vpii;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<string>> vvs;
+typedef stack<int> stki;
+typedef queue<int> qi;
+typedef deque<int> dqi;
+typedef queue<pair<int, int>> qpii;
+typedef unordered_map<int, int> umpii;
+typedef unordered_map<int, vi> umpiv;
+typedef set<int> si;
+typedef unordered_set<int> usi;
+vpii steps = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vvi ans;
+        
+        queue<TreeNode*> q;
+        
+        if(root) q.push(root);
+        
+        while(!q.empty()){
+            int size = q.size();
+            vi temp;
+            while(size--){
+                root = q.front();
+                q.pop();
+                temp.push_back(root->val);
+                if (root->left)
+                    q.push(root->left);
+                if(root->right)
+                    q.push(root->right);
+            }
+            
+            ans.push_back(temp);
+        }
+        
+        return ans;
+    }
+};
