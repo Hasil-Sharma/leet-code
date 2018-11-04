6b376a4cb63c3ffa2380bba11cced36a59d8e38d
Hasil Sharma <hasilsharma7@gmail.com>
2018-11-03 22:17:26 -0600

    5. Longest Palindromic Substring - O(n^2) & O(n^2)

---
+++ longest-palindromic-substring-3.cpp
@@ -0,0 +1,56 @@
+// https://leetcode.com/problems/longest-palindromic-substring/description/
+
+#define mp(A, B) make_pair(A, B)
+#define ff(i, m, n) for(i = m; i < n; i++)
+#define ffr(i, m, n) for(i = m; i >= n; i--)
+#define ss(v) sort(v.begin(), v.end())
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<char>> vvc;
+typedef vector<pair<int, int>> vpii;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<string>> vvs;
+typedef stack<int> stki;
+typedef queue<int> qi;
+typedef deque<int> dqi;
+typedef queue<pair<int, int>> qpii;
+typedef unordered_map<int, int> umpii;
+typedef unordered_map<int, vi> umpiv;
+typedef set<int> si;
+typedef unordered_set<int> usi;
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.length(), i, start = 0, mm = 1, k, j;
+        vvb dp(n, vb(n, false));
+        
+        ff(i, 0, n){
+            dp[i][i] = true;
+        }
+        
+        ff(i, 0, n - 1){
+            if (s[i] == s[i+1]){
+                dp[i][i+1] = true;
+                start = i;
+                mm = 2;
+            }
+        }
+        
+        ff(k, 3, n+1){
+            ff(i, 0, n - k + 1){
+                j = i + k - 1;
+                if(dp[i+1][j-1] && s[i] == s[j]){
+                    dp[i][j] = true;
+                    start = i;
+                    mm = k;
+                }
+            }
+        }
+        
+        return s.substr(start, mm);
+    }
+};
