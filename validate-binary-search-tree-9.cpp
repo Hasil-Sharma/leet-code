dd2286e3b0566d26c7ecdca4f5da7bbf2bee59e6
Hasil Sharma <hasilsharma7@gmail.com>
2018-11-18 19:17:30 -0700

    98. Validate Binary Search Tree - O(n

---
+++ validate-binary-search-tree-9.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/validate-binary-search-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        stack<TreeNode*> stk;
+        TreeNode* prev = NULL;
+        
+        while(!stk.empty() || root){
+            while(root){
+                stk.push(root);
+                root = root->left;
+            }
+            
+            root = stk.top();
+            stk.pop();
+            if (prev && prev->val >= root->val) return false;
+            prev = root;
+            root = root->right;
+        }
+        
+        return true;
+    }
+};
