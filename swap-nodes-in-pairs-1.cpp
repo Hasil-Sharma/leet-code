5f1d4c36ab6d191373ce736df8fa427cde9d3438
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-12-15 18:45:13 -0700

    24. Swap Nodes in Pairs - O(n) & O(n)

---
+++ swap-nodes-in-pairs-1.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/swap-nodes-in-pairs/description/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode* ans = head->next, *temp = head->next->next;
+        
+        ans->next = head;
+        head->next = swapPairs(temp);
+        
+        return ans;
+    }
+};
