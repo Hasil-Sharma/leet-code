517c37a9aa57bac89d6ba49b72d423d343c62554
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-12-31 18:52:38 +0530

    141. Linked List Cycle - O(n) & O(1)

---
+++ linked-list-cycle-5.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/linked-list-cycle/description/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head, *fast = head && head->next ? head->next->next : NULL;
+        
+        
+        while(slow != fast){
+            slow = slow->next;
+            fast = fast && fast->next ? fast->next->next : NULL;
+        }
+        
+        return fast && slow == fast;
+    }
+};
