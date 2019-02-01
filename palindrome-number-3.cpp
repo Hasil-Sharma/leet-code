b44cf9b0909dfb42f46311691706024a7579e677
Hasil Sharma <hasilsharma7@gmail.com>
2019-01-31 23:17:04 -0700

    9. Palindrome Number - O(log n) & O(1)

---
+++ palindrome-number-3.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/palindrome-number/description/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x && x%10 == 0)) return false;
+        
+        int y = 0;
+        
+        while(x > y){
+            y = y*10 + (x%10);
+            x /= 10;
+        }
+        
+        return x == y || x == y/10;
+    }
+};
